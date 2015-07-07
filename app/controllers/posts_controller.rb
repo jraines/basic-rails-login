9cd8413f49a4e9e73fb7e3f463cc44b5df8d929e
Jeremy Raines <jraines@gmail.com>
2015-07-07 17:16:51 -0400

    User signup and post creation - also make current_user available in controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,16 @@
 class PostsController < ApplicationController
+
+  def index
+    @posts = Post.order(id: :desc)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.create params[:post]
+    redirect_to posts_path
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create params[:user]
+    flash[:notice] = "Thanks for Signing Up"
+    redirect_to posts_path
+  end
 end
