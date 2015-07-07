df956cb124787cabb73497508caa6369bde6f07e
Jeremy Raines <jraines@gmail.com>
2015-07-07 16:37:33 -0400

    Add birthday column to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150707203248) do
+ActiveRecord::Schema.define(version: 20150707203651) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20150707203248) do
     t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "birthday"
   end
 
 end
