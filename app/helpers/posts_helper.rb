b1b393ad45f0185847f347e4660aa9210b3a72fe
Jeremy Raines <jraines@gmail.com>
2015-07-10 11:03:14 -0400

    Extract helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,11 @@
 module PostsHelper
+
+  def publish_or_count(posts)
+    if current_user
+      link_to "Create New Post", new_post_path
+    else
+      "There are #{pluralize(posts.count, 'post')}"
+    end
+  end
+
 end
