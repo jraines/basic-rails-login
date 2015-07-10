50258da95b9c6844a73a9438960b15a1ff06d4d1
Jeremy Raines <jraines@gmail.com>
2015-07-10 14:03:57 -0400

    Raise error when trying to follow a blocked user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,19 +5,19 @@ class User < ActiveRecord::Base
   has_many :followings_to, class_name: "Following", foreign_key: "follower_id"
   has_many :followings_from, class_name: "Following", foreign_key: "followed_id"
 
-  has_many :followed_users, through: :followings_from, source: :followed
-  has_many :followers, through: :followings_from, source: :follower
+  has_many :followed_users, through: :followings_to, source: :followed
+  has_many :followers, through: :followings_from
 
-  has_many :blockings
 
   has_many :blocked_to, class_name: "Blocking", foreign_key: "blocker_id"
-  has_many :blocking_from, class_name: "Blocking", foreign_key: "blocked_id"
+  has_many :blocked_from, class_name: "Blocking", foreign_key: "blocked_id"
 
   has_many :blocked_users, through: :blocked_to, source: :blocked
   has_many :blocking_users, through: :blocked_from, source: :blocker
 
 
   def follow(user)
+    raise "Blocked!" if user.blocked_users.find(self)
     user.followers << self
   end
 
